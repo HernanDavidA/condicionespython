e1f7974d6e416b8db86b0cc9e000109525508e6b
r3l4x1n <nfzapataca@cesde.net>
2023-08-18 10:14:57 -0500

    }

---
+++ condiciones.py
@@ -0,0 +1,7 @@
+edadUsuario=int(input("Digita tu edad: "))
+
+if edadUsuario >= 18:
+    print("Bienvenido a la disco")   
+else:
+    print("Eres menor de edad")
+    print("Pa la casa gorda puta")
---
+++ condiciones2.py
@@ -0,0 +1,7 @@
+temperatura = int(input("Ingrese la temperatura: "))
+gradosAlcohol = int(input("Ingrese los grados de alcohol"))
+
+if temperatura >= 50 or gradosAlcohol >=5:
+    input("Alerta, la bebida está en peligro: ")
+else: 
+    input("Trabajamos en condiciones aceptables")
